bf9ff3bd2c9f10ce815746214a5bd6463798a3d9
HirushaR <hirusharandunu11@gmail.com>
2019-07-04 14:54:41 +0530

    add the new files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,c;
+            Console.WriteLine("enter first number");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            b = int.Parse(Console.ReadLine());
+
+            Class1 ca = new Class1(); // this is creating object
+            c =  ca.getsum(a,b); // use the getsum method in class1
+            Console.WriteLine("" + c);
+            Console.ReadLine();
+
+        }
+    }
+
+   
+}
